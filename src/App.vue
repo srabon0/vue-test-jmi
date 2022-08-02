2afcf8dda139d6ebb614e9df43223c4901d99d89
srabon0 <srabonemam4@gmail.com>
2022-08-02 23:37:52 +0600

    primary  commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+    id="sideNav"
+  >
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-none d-lg-block"
+        ><img
+          class="img-fluid img-profile mx-auto mb-2"
+          src="assets/img/JERPLOGO2.png"
+          alt=""
+          style="border: none"
+      /></span>
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">Instructions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#">BLOG</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#"><hr /></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#experience"
+            >Example Blog</a
+          >
+        </li>
+        <LeftMenu :blogs="blogs" />
+        <!-- vue component -->
+        <!-- <li class="nav-item" :key="blog.id" v-for="blog in blogs">
+          <a :href="'#' + blog.id"> {{ blog.title }} </a>
+        </li> -->
+      </ul>
+    </div>
   </nav>
-  <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import LeftMenu from "./components/LeftMenu.vue";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    LeftMenu,
+  },
+  data() {
+    return {
+      blogs: null,
+      allBlogs: this.getAllblogs(),
+    };
+  },
+  created() {
+    axios
+      .get("http://vue-test.gingerbd.com/api/blogs")
+      .then((response) => {
+        this.blogs = response.data.blogs;
+        console.log(this.blogs);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  methods: {
+    async getAllblogs() {
+      var vas = await axios.get("http://vue-test.gingerbd.com/api/all-blogs");
+      this.allBlogs = vas.data;
+    },
+  },
+};
+</script>
---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="nav-item" :key="blog.id" v-for="blog in blogs">
+    <a class="nav-link" :href="'#' + blog.id"> {{ blog.title }} </a>
+  </li>
+</template>
+<script>
+export default {
+  name: "LeftMenu",
+  props: {
+    blogs: Array,
+  },
+};
+</script>
